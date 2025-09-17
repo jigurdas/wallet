615a20c22536883eb1a7d81cf717eed717ae09a9
jigurdas <step.dima209@gmail.com>
2025-09-17 18:44:32 +0300

    connect to db

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,5 +1,16 @@
 package app
 
+import (
+	"log"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
 func main() {
+	db, err := pgxpool.New(ctx, "postgresql://user:password@localhost:5432/walletdb")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
 
 }
