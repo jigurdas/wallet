369eefcc3e081a2958cacbb857ff242399d612c8
jigurdas <step.dima209@gmail.com>
2025-09-17 21:49:32 +0300

    add code for auth handler

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,12 +1,14 @@
 package app
 
 import (
+	"context"
 	"log"
 
 	"github.com/jackc/pgx/v5/pgxpool"
 )
 
 func main() {
+	ctx := context.Background()
 	db, err := pgxpool.New(ctx, "postgresql://user:password@localhost:5432/walletdb")
 	if err != nil {
 		log.Fatal(err)
--- internal/handler/auth_handler.go
+++ internal/handler/auth_handler.go
@@ -1,1 +1,45 @@
 package handler
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"wallet/internal/entitty"
+	"wallet/internal/usecase"
+)
+
+type UserHandler struct {
+	uc *usecase.UserUseCase
+}
+
+func NewUserHandler(uc *usecase.UserUseCase) *UserHandler {
+	return &UserHandler{uc: uc}
+}
+
+func (h *UserHandler) Register(w http.ResponseWriter, r *http.Request) {
+	var req struct {
+		Username string `json:"username"`
+		Password string `json:"password"`
+	}
+	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+		http.Error(w, "Invalid request payload", http.StatusBadRequest)
+		return
+	}
+
+	user := &entitty.User{
+		Username: req.Username,
+		Password: req.Password,
+	}
+
+	id, err := h.uc.Register(r.Context(), user)
+	if err != nil {
+		http.Error(w, "Failed to register user", http.StatusInternalServerError)
+		return
+	}
+	w.WriteHeader().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(map[string]any{
+		"id":       id,
+		"username": req.Username,
+		"status":   "created",
+	})
+}
